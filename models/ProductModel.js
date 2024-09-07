79251523151eb406675c5769ac9c7c279d31c296
anurpeljto <anurpeljto@stu.ibu.edu.ba>
2024-09-07 17:44:11 +0200

    Create product and category models

---
+++ models/ProductModel.js
@@ -0,0 +1,17 @@
+const mongoose = require('mongoose');
+
+const ProductSchema = mongoose.Schema({
+    name: {
+        type: String,
+        required: [true, 'Product name is required'],
+        minLength: 3,
+        maxLength: 15,
+        trim: true,
+    },
+
+    category: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'Category',
+        required: [true, 'Category is required']
+    }
+})
